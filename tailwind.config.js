43aaa6633a72f8d2bda02460b5ca68797f41efde
ponurakk <p0unrakg@int.pl>
2023-12-07 20:54:41 +0100

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{php,html}", "./components/**/*.{php,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- views/index.view.php
+++ views/index.view.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="/static/index.css" rel="stylesheet">
 </head>
 
 <body>
