50330c32cf8cb6a1ff4647742f334dcb1b5da9d6
spaceKumpel <michalpociask11@o2.pl>
2023-12-13 20:44:05 +0100

    moje zmiany

---
+++ views/login/login.view.php
@@ -0,0 +1,15 @@
+<?php
+    include "./lib/utils.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="./static/index.css" rel="stylesheet">
+    </head>
+    <body class="bg-sky-700">
+        <img href="https://blenderartists.org/uploads/default/original/4X/d/7/1/d7185be4c0b6eb12b563e744376c0476c42cc3a9.png">
+    </body>
+</html>
